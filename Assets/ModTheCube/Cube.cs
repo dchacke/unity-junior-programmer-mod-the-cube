67907e22803cd5618c97e93bb77c1a0ad556d0cc
Dennis Hackethal <dh@dennishackethal.com>
2023-12-17 22:32:34 -0800

    Change rotation of cube

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -18,6 +18,6 @@ public class Cube : MonoBehaviour
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(new Vector3(10.0f, 10.0f, 10.0f) * Time.deltaTime);
     }
 }
