663aba66986bda0939b82f86615405f5253bbca4
Dennis Hackethal <dh@dennishackethal.com>
2023-12-17 23:44:47 -0800

    Housekeeping

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -13,14 +13,29 @@ public class Cube : MonoBehaviour
     
     void Update()
     {
+        Rotate();
+        Pulsate();
+        ChangeColor();
+    }
+
+    private void Rotate()
+    {
         transform.Rotate(new Vector3(10.0f, 10.0f, 10.0f) * Time.deltaTime * 10);
+    }
 
+    private void Pulsate()
+    {
         float scale = Mathf.Sin(Time.time) + 1;
-        transform.localScale = new Vector3(scale, scale, scale);
 
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+    private void ChangeColor()
+    {
         float r = Mathf.Abs(Mathf.Sin(Time.time) % 1);
         float g = Mathf.Abs(Mathf.Sin(Time.time + 1) % 1);
         float b = Mathf.Abs(Mathf.Sin(Time.time + 2) % 1);
+
         Renderer.material.color = new Color(r, g, b);
     }
 }
